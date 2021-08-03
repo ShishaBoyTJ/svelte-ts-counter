d5a342dbb10a6eeba5991e3a76900c0f6a48b324
tabatajunya <lovebaseball697@gmail.com>
2021-08-04 04:31:31 +0900

    [add]カウンターコンポーネント・ページ追加

---
+++ src/components/pages/index.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+import CounterContainer from "../templates/CounterContainer.svelte";
+import Button from "../parts/Button.svelte"
+import { counterList } from "../../store/counterList"
+import _ from "lodash"
+
+interface defaultCounterList {
+  title:string,
+  count:number
+}
+
+const counterConstant = {
+  title: "new",
+  count:0
+}
+
+$:reactiveCounterList = $counterList
+$:countSum = $counterList.reduce(function(sum:number, element:defaultCounterList){
+  return sum + element.count;
+}, 0);
+const onClickAddCounterContainer = () => {
+  $counterList =[...$counterList, _.cloneDeep(counterConstant)]
+}
+const handleOnClickDelete = (value :any) => {
+  // @note value.countが0の時リストが消えないので手を加えた
+  countSum = countSum - value.count 
+  countSum += 1
+  $counterList.splice(value.index, 1)
+}
+
+</script>
+<div class="">
+  {#each $counterList as item, index}
+    {index}<CounterContainer index={index} bind:title={item.title}  bind:count={item.count} on:delete={e => handleOnClickDelete(e.detail)}/>
+  {/each}
+  <Button on:click={onClickAddCounterContainer}/>
+  {#each reactiveCounterList as item}
+    {item.title}
+    {item.count}
+  {/each}
+  some of count:{countSum}
+</div>
+<style></style>
---
+++ src/components/templates/CounterContainer.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+  import InputText from "../parts/InputText.svelte"
+  import Button from "../parts/Button.svelte"
+  // import { count } from '../../store/count';
+  // let defaultTitle = 'new'
+  // let count = 0;
+  const dispatch = createEventDispatcher();
+  export let index :number;
+  export let count :number;
+  export let title :string;
+  const onClickIncrement = () => {
+    // count.update(n => n + 1);
+    count += 1
+  };
+  const onClickDecriment = () => {
+    // count.update(n => n - 1);
+    count -= 1
+  }
+  const onClickReset = () => {
+    // count.set(0)
+    count = 0
+  }
+  const onClickDelete = () => {
+    count += 1
+    const value = {
+      count,
+      index
+    }
+    dispatch('delete', value)
+    count = 0
+  }
+</script>
+
+<div class="flex">
+  <InputText bind:value={title}/>
+  <Button on:click={onClickIncrement} value={"+"} />
+  <Button on:click={onClickDecriment} value={"-"}/>
+  <Button on:click={onClickReset} value={"0"}/>
+  <Button on:click={onClickDelete} value={"×"}/>
+</div>
+
+<style lang="scss">
+.flex{
+  display: flex;
+  justify-content: center;
+}
+</style>
